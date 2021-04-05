a2eefeaf5863d4f8c36f5c53b6f6251384059bc7
Marcelo Rocha <celo.rmr@gmail.com>
2021-04-05 20:31:54 -0300

    first commit

---
+++ Songs/Unknown Music.rb
@@ -0,0 +1,73 @@
+play :A4
+sleep 0.5
+play :A4
+sleep 0.5
+play :A4
+sleep 0.5
+play :B4
+sleep 0.5
+play :C5
+sleep 1
+play :B4
+sleep 2
+
+# part 2
+
+play :G4
+sleep 0.5
+play :A4
+sleep 0.5
+play :B4
+sleep 1
+play :C5
+sleep 0.5
+play :B4
+sleep 1
+play :A4
+sleep 1
+play :G4
+sleep 1
+play :G4
+sleep 0.5
+play :A4
+sleep 1
+play :A4
+sleep 1.5
+
+# part 3
+
+play :A4
+sleep 0.5
+play :A4
+sleep 0.5
+play :G4
+sleep 0.5
+play :A4
+sleep 1
+play :A4
+sleep 0.5
+play :G4
+sleep 0.5
+play :A4
+sleep 1
+play :A4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 1.5
+
+# part 4
+
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :E4
+sleep 0.5
+play :E4
+sleep 0.5
+play :Fs4
+sleep 2
